9e73510705d5e8f68cbc08532b3d04712bd0b1da
Nike2406 <locus2@mail.ru>
2022-10-13 22:30:00 +0300

    fullfilled fragments

--- app/src/main/java/com/flatlyx/hackaton/presentation/MainActivity.kt
+++ app/src/main/java/com/flatlyx/hackaton/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.flatlyx.hackaton
+package com.flatlyx.hackaton.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.flatlyx.hackaton.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

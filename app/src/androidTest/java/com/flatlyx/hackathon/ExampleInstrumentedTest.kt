e0eec98410c176fddbcfa6133c97c39aef3b6c8a
Sergey Lavrov <lavrov-sergey@yandex.ru>
2022-10-14 21:53:11 +0300

    added configs for unit-tests and e2e-tests

--- app/src/androidTest/java/com/flatlyx/hackathon/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/flatlyx/hackathon/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.flatlyx.hackaton", appContext.packageName)
+        assertEquals("com.flatlyx.hackathon", appContext.packageName)
     }
 }
